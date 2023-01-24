709ee1f3963c9cbb95ca6297a281fce4fbde759d
Florian Ludwig <florian.ludwig-hro@gmx.de>
2023-01-24 17:38:44 +0100

    fixed Code Smells

--- frontend/src/Auth/Auth.tsx
+++ frontend/src/Auth/Auth.tsx
@@ -18,12 +18,8 @@ export default function Auth (
     const {user, isReady} = useAuth();
 
     const navigate = (
-        <Navigate to={isReady && !user ? `/login?redirect=${encodeURIComponent(location.pathname + location.search)}` : "/"}
-        />);
+        <Navigate to={isReady && !user ? `/login?redirect=${encodeURIComponent(location.pathname + location.search)}` : "/"}/>
+    );
 
-    return !isReady
-        ? null
-        : (user && roles.includes(user.role)
-            ? <>{children}</>
-            : (shouldRedirect ? navigate : null));
+    return !isReady ? null : (user && roles.includes(user.role) ? <>{children}</> : (shouldRedirect ? navigate : null));
 }
