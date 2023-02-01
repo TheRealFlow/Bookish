a7535375ffe069959c80385e00c5c2d7371823bb
Florian Ludwig <florian.ludwig-hro@gmx.de>
2023-02-01 17:21:22 +0100

    Fixed Search-Function and Added Buttons

--- frontend/src/pages/SearchBooks.tsx
+++ frontend/src/pages/SearchBooks.tsx
@@ -2,13 +2,15 @@ import React, {useState} from "react";
 import axios from "axios";
 import ApiKey from "../config";
 import {BookAPI} from "../Types/BookAPI";
-import {Box, Container, IconButton, TextField, Typography} from "@mui/material";
+import {Box, Button, Container, IconButton, TextField, Typography} from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+import {useNavigate} from "react-router-dom";
 
 export default function SearchBooks() {
     const [search, setSearch] = useState("");
     const [result, setResult] = useState([]);
     const apikey = ApiKey();
+    const navigate = useNavigate();
 
     const handleChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
         setSearch(event.target.value);
@@ -45,7 +47,9 @@ export default function SearchBooks() {
 
             <Box>
                 {result.map((item: BookAPI) => (
-                    <Container sx={{ m: 5, p: 5, border: 1, borderRadius: "5px" }}>
+
+                        <Container sx={{ m: 5, p: 5, border: 1, borderRadius: "5px" }}>
+
                             <Box>
                                 {item.volumeInfo.imageLinks === undefined ?
                                     <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png"} alt={"Not Available"}/> :
@@ -55,7 +59,7 @@ export default function SearchBooks() {
                                          alt={item.volumeInfo.title}>
                                     </Box>}
                             </Box>
-                            <Box sx={{borderBottom: 1}}>
+                            <Box sx={{borderBottom: 1}} key={item.id}>
                                 <>
                                     {item.volumeInfo.title === undefined ?
                                         <Typography>Unknown Title</Typography> :
@@ -71,7 +75,7 @@ export default function SearchBooks() {
                                     <Typography>No description available</Typography> :
                                 <Typography>{item.volumeInfo.description}</Typography>}
                             </Box>
-                            <Box>
+                            <Box sx={{borderBottom: 1, my: 2}}>
                                 <>
                                     {item.volumeInfo.publishedDate === undefined ?
                                         <Typography>Unknown Publication Date</Typography> :
@@ -80,11 +84,24 @@ export default function SearchBooks() {
                                     {item.volumeInfo.pageCount === undefined ?
                                         <Typography>Unknown Pages</Typography> :
                                     <Typography>Pages: {item.volumeInfo.pageCount}</Typography>}
+
+                                    {item.volumeInfo.categories === undefined ?
+                                        <Typography>Unknown Category</Typography> :
+                                    <Typography>Category: {item.volumeInfo.categories}</Typography>}
+
+                                    {item.volumeInfo.industryIdentifiers === undefined ?
+                                        <Typography>Unknown ISBN</Typography> :
+                                        <Typography>{item.volumeInfo.industryIdentifiers[0].type}: {item.volumeInfo.industryIdentifiers[0].identifier}</Typography>}
                                 </>
                             </Box>
-                    </Container>
-                    ))}
+
+                            <Button variant={"contained"}>Add to my List</Button>
+
+                        </Container>
+                ))}
             </Box>
+
+            <Button variant={"outlined"} onClick={() => navigate("/")}>Home</Button>
         </>
     )
 
