8eae133f0922893028144e18e7ab429810355fb6
Florian Ludwig <florian.ludwig-hro@gmx.de>
2023-02-08 13:16:28 +0100

    Fixed image display + modal for user search

--- frontend/src/pages/ProfilPage.tsx
+++ frontend/src/pages/ProfilPage.tsx
@@ -1,91 +1,48 @@
-import React from "react";
-import axios from "axios";
-import {toast} from "react-toastify";
-import {Button, Paper, TextField, Typography} from "@mui/material";
-import {EditOutlined, PhotoCamera} from "@mui/icons-material";
+import React, {useState} from "react";
+import {Button, Dialog, DialogActions, Paper, Typography} from "@mui/material";
 import Box from "@mui/material/Box";
 import NavBar from "../Components/NavBar";
+import useAuth from "../hooks/useAuth";
+import {green} from "@mui/material/colors";
+import EditIcon from '@mui/icons-material/Edit';
+import ChangeImage from "../Components/ChangeImage";
+import useBooks from "../hooks/useBooks";
 
 export default function ProfilePage() {
-    const [file, setFile] = React.useState<File | null>(null);
-    /*const [imgPreview, setImgPreview] = React.useState<string | null>(null);*/
-    const fileInputRef = React.useRef<HTMLInputElement>(null);
+    const {books} = useBooks();
+    const {user} = useAuth();
+    const [showEditForm, setShowEditForm] = useState(false);
+
+    const handleShowEditForm = () => {
+        setShowEditForm(true);
+    }
+    const handleCloseEditForm = () => {
+        setShowEditForm(false);
+    }
 
     return (
         <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
             <NavBar/>
             <Typography variant={"h4"} sx={{my: 1.5}}>My Profile</Typography>
 
-            <Box sx={{width: "200px", height: "200px", borderRadius: "50%", backgroundColor: "#7D93A6", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
-                <img src={""} alt={"preview"}/>
-            </Box>
+            <img src={"/images/"+user?.imageId} alt={"preview"} style={{width: "200px", height: "200px", borderRadius: "50%"}}/>
 
-                {/*{imgPreview === null ?
-                    <Box sx={{width: "200px", height: "200px", borderRadius: "50%", backgroundColor: "#7D93A6", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
-                        <Button variant={"contained"} endIcon={<PhotoCamera/>} onClick={() => {
-                            fileInputRef.current?.click();
-                        }}> SELECT IMAGE</Button>
-                    </Box> :
-                    <img
-                        style={{width: "200px", height: "200px", borderRadius: "50%"}}
-                        src={imgPreview}
-                        alt={"preview"}
-                    />
-                }*/}
+            <Button onClick={handleShowEditForm} sx={{m: 2, bgcolor: green[500]}} variant={"contained"} endIcon={<EditIcon/>}>Change Image</Button>
+            <Dialog open={showEditForm} onClose={handleCloseEditForm}>
+                {showEditForm && (
+                    <ChangeImage/>
+                )}
+                <DialogActions>
+                    <Button onClick={handleCloseEditForm} variant={"outlined"}>Close</Button>
+                </DialogActions>
+            </Dialog>
 
-            <form onSubmit={async (e) => {
-                e.preventDefault();
-                if (file) {
-                    const formData = new FormData();
-                    formData.append("file", file);
+            <Paper elevation={3} sx={{m: 2, display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", width: 350}}>
+                <Typography variant={"h6"} sx={{my: 1.5}}>Username: {user?.username}</Typography>
+                <Typography variant={"h6"} sx={{my: 1.5}}>Books: {books.length}</Typography>
+                <Typography variant={"h6"} sx={{my: 1.5}}>Genres: {books.map(book => book.genre).join(", ")}</Typography>
+            </Paper>
 
-                    axios.post("/images", formData)
-                        .then(() => toast.success("Image uploaded successfully!"))
-                        .catch((error) => toast.error(error.message));
-                }
-                }}>
-                <Button sx={{marginTop: 2, marginLeft: 14}} variant={"outlined"} endIcon={<EditOutlined/>} onClick={() => {
-                    fileInputRef.current?.click();
-                }}>Change Image</Button>
-
-                <input
-                    ref={fileInputRef}
-                    style={{ display: "none" }}
-                    type={"file"}
-                    onChange={(e) => {
-                        if (!e.target.files || e.target.files.length < 1) {
-                            toast.error("No image selected", {
-                                position: "top-center",
-                                autoClose: 3000,
-                                hideProgressBar: false,
-                                closeOnClick: true,
-                                pauseOnHover: true,
-                            })
-                            setFile(null);
-                            /*setImgPreview(null);*/
-                            return;
-                        }
-                        setFile(e.target.files[0]);
-                        const reader = new FileReader();
-                        reader.addEventListener("load", () => {
-                            /*setImgPreview(reader.result as string);*/
-                        }, false);
-                        if (file) {
-                            reader.readAsDataURL(file);
-                        }
-                    }}
-                    accept={"image/jpeg, image/png"}
-                />
-
-                <Paper elevation={3} sx={{m: 2, display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", width: 350}}>
-                    {/*<TextField variant={"standard"} label={"Change Username"} sx={{m: 1}}/>
-                    <TextField variant={"standard"} label={"New Password"} sx={{m: 1}}/>
-                    <TextField variant={"standard"} label={"Confirm Password"} sx={{m: 1}}/>*/}
-
-                    <Button sx={{m: 2.5}} variant={"contained"} type={"submit"}>Save Changes</Button>
-                </Paper>
-            </form>
-
-            </Box>
+        </Box>
     );
 }
