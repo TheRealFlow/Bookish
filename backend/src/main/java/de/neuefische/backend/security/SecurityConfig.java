2b624f04058357c166bf8c511103a95b20a82583
Florian Ludwig <florian.ludwig-hro@gmx.de>
2023-02-22 11:51:52 +0100

    Fixed Login PopUp

--- backend/src/main/java/de/neuefische/backend/security/SecurityConfig.java
+++ backend/src/main/java/de/neuefische/backend/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package de.neuefische.backend.security;
 
-
 import de.neuefische.backend.appuser.AppUser;
 import de.neuefische.backend.appuser.AppUserService;
 import lombok.RequiredArgsConstructor;
@@ -8,11 +7,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Optional;
 
 @Configuration
@@ -24,7 +28,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
         return http
                 .csrf().disable()
-                .httpBasic().and()
+                .httpBasic().authenticationEntryPoint(new PreventBasicAuthPopupEntryPoint()).and()
                 .authorizeHttpRequests()
                 .antMatchers(HttpMethod.POST, "/api/user").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/user/login").permitAll()
@@ -54,4 +58,11 @@ public class SecurityConfig {
         };
     }
 
+    private static class PreventBasicAuthPopupEntryPoint implements AuthenticationEntryPoint {
+        @Override
+        public void commence(HttpServletRequest request, HttpServletResponse response,
+                             AuthenticationException authException) throws IOException {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+        }
+    }
 }
