2e29c2ffdd46c2d73f125d0956de750d7498cb9f
Florian Ludwig <florian.ludwig-hro@gmx.de>
2023-02-22 10:55:15 +0100

    Fixed Delete Account Function

--- frontend/src/pages/ProfilPage.tsx
+++ frontend/src/pages/ProfilPage.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {Button, Dialog, DialogActions, Fab, Paper, Typography} from "@mui/material";
+import {Button, Dialog, DialogActions, DialogTitle, Fab, Paper, Typography} from "@mui/material";
 import Box from "@mui/material/Box";
 import NavBar from "../components/NavBar";
 import useAuth from "../hooks/useAuth";
@@ -9,13 +9,18 @@ import useBooks from "../hooks/useBooks";
 import useFriends from "../hooks/useFriends";
 import useBookClubs from "../hooks/useBookClubs";
 import {ArrowBack} from "@mui/icons-material";
+import useUser from "../hooks/useUser";
+import {useNavigate} from "react-router-dom";
 
 export default function ProfilePage() {
     const {books} = useBooks();
     const {user} = useAuth();
+    const {deleteUser} = useUser();
     const {friends} = useFriends();
     const {bookClubs} = useBookClubs();
     const [showEditForm, setShowEditForm] = useState(false);
+    const [open, setOpen] = useState(false);
+    const navigate = useNavigate();
 
     const handleShowEditForm = () => {
         setShowEditForm(true);
@@ -23,6 +28,16 @@ export default function ProfilePage() {
     const handleCloseEditForm = () => {
         setShowEditForm(false);
     }
+    const handleDelete = (id: string | undefined) => {
+        deleteUser(id);
+        navigate("/login");
+    }
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     return (
         <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
@@ -50,7 +65,21 @@ export default function ProfilePage() {
                 <Typography sx={{m: 1, p: 1, fontWeight: 600}}><Typography color={"secondary"}>Friends: </Typography>{friends.length}</Typography>
                 <Typography sx={{m: 1, p: 1, fontWeight: 600}}><Typography color={"secondary"}>Book-Clubs: </Typography>{bookClubs.length}</Typography>
                 <Typography sx={{m: 1, p: 1, fontWeight: 600}}><Typography color={"secondary"}>Books: </Typography>{books.length}</Typography>
-                <Button variant={"contained"} color={"error"} sx={{m: 4}}>Delete Account</Button>
+
+                <Button variant={"contained"} color={"error"} sx={{m: 4}} onClick={handleClickOpen}>Delete Account</Button>
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">{"Are you sure you want to delete your Account?"}</DialogTitle>
+                    <DialogActions>
+                        <Button onClick={() => handleDelete(user?.id)} variant={"outlined"} color={"secondary"} autoFocus>Yes</Button>
+                        <Button onClick={handleClose} color={"secondary"}>No</Button>
+                    </DialogActions>
+                </Dialog>
+
             </Paper>
 
             <Fab color="primary" aria-label="back">
